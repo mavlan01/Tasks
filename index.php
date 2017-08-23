a6cd25544389ee250c0888436ec6956413c2fcb0
mavlan01 <31222861+mavlan01@users.noreply.github.com>
2017-08-23 18:38:01 +0300

    first

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+ $number = 45;
+ $float = -3.25;
+ $string = "Hello World";
+ $bool = true;
+ echo  "Целочисленная переменная = $number";
+echo  "<br />";
+echo  gettype($number);
+echo  "<br />";
+ echo  "Переменная с плавающей точкой = $float";
+echo  "<br />";
+ echo  gettype($float);
+echo  "<br />";
+ echo  "Строковая переменная = $string";
+ echo  "<br />";
+echo  gettype($string);
+echo  "<br />";
+ echo  "Булева переменная= $bool";
+echo  "<br />";
+echo  gettype($bool);
+echo  "<br />";
+ ?>
