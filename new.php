dc630a7d6c968f13d9e93abda17ce8ae9dbbf330
mavlan01 <31222861+mavlan01@users.noreply.github.com>
2017-09-20 23:20:15 +0300

    New

---
+++ new.php
@@ -0,0 +1,41 @@
+<form method="post" action="<?=$_SERVER['PHP_SELF']?>">
+    a:<input type="number" name="a">
+    <select name="select"><option>Выберите из списка</option>
+        <option>-</option>
+        <option>+</option>
+        <option>*</option>
+        <option>/</option>
+    </select>
+    b:<input type="number" name="b">
+    <input type='submit' name='vichislit' value='Вычислить'>
+    <input type="reset" name='ochistit' value='Отменить'>
+</form>
+<?php
+Class kalkul
+{
+    public function kalk($a, $b, $select)
+    {
+        if ($_POST['vichislit']) {
+            if ($select == '+') {
+                $c = $a + $b;
+                echo "сумма=" . $c . '<br>';
+            }
+            if ($select == '-') {
+                $c = $a - $b;
+                echo "разность=" . $c . '<br>';
+            }
+            if ($select == '*') {
+                $c = $a * $b;
+                echo "умножение=" . $c . '<br>';
+            }
+            if ($select == '/') {
+                $c = $a / $b;
+                echo "деление=" . $c . '<br>';
+            }
+        }
+    }
+}
+$kalk=new kalkul();
+$kalk->kalk($_POST['a'], $_POST['b'], $_POST['select']);
+
+?>
